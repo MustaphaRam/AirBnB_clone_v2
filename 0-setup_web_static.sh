81575b6d28da90e1af52a256b5c0881f956a0f37
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-10 23:31:21 +0100

    Airbnb_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 # Sets up a web server for deployment of web_static.
 
-apt-get update
-apt-get install -y nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
 printf %s "server {
     listen 80 default_server;
@@ -21,11 +27,11 @@ printf %s "server {
 
     location /hbnb_static {
         alias /data/web_static/current;
-        index index.html index.htm;
+        index index.html index.html;
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 https://github.com/MustaphaRam;
     }
 
     error_page 404 /404.html;
@@ -35,4 +41,4 @@ printf %s "server {
     }
 }" > /etc/nginx/sites-available/default
 
-service nginx restart
+sudo service nginx restart
