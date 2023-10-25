01827c5e7253d1dc50d85eabb297d18f2426dc31
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-26 00:02:13 +0100

    AirBnB_clone_v2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 from flask import Flask
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-
 
 @app.route('/')
 def hello_hbnb():
@@ -26,4 +27,3 @@ def c_is_fun(text):
 if __name__ == '__main__':
     app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
-
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,6 +2,7 @@
 """ Script that starts a Flask web application """
 from flask import Flask
 
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -35,4 +36,3 @@ def python_is_cool(text='is cool'):
 if __name__ == '__main__':
     app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
-
